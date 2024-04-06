1015ec6fa537b17061a875c380662d9b7548118f
log.club.sonia <log.club.sonia@etsmtl.net>
2024-04-06 15:35:19 -0400

    fichiers de test

--- train.py
+++ train.py
@@ -1,26 +1,41 @@
-from ultralytics.yolo.data.converter import convert_coco
 import shutil
 import json
 from ultralytics import YOLO
+from ultralytics.data.converter import convert_coco
+import torch
+import gc
+
 
 # Train the model
 if __name__ == "__main__":
-    convert_coco(labels_dir='datasets/', cls91to80=False)
-    with open("datasets/train.json", "r") as f:
-        coco_json = json.load(f)
-    yaml_content = """path: ./
-train: images/train
-val: images/val
-names:
-"""
-    for subclass in coco_json["categories"]:
-        yaml_content += f"  {subclass['id']-1}: {subclass['name']}\n"
-    with open("./yolo_labels/train.yaml", "w") as f:
-        f.write(yaml_content)
+#     convert_coco(labels_dir='datasets/', cls91to80=False)
+#     with open("datasets/train.json", "r") as f:
+#         coco_json = json.load(f)
+#     yaml_content = """path: ./
+# train: images/train
+# val: images/val
+# names:
+# """
+#     for subclass in coco_json["categories"]:
+#         yaml_content += f"  {subclass['id']-1}: {subclass['name']}\n"
+#     with open("./yolo_labels/train.yaml", "w") as f:
+#         f.write(yaml_content)
 
-    shutil.rmtree("./datasets/labels", ignore_errors=True)
-    shutil.move("./yolo_labels/labels", "./datasets/labels")
-
+#     shutil.rmtree("./datasets/labels", ignore_errors=True)
+#     shutil.move("./yolo_labels/labels", "./datasets/labels")
+#     model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
     # Load a model
-    model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
-    model.train(data='./yolo_labels/train.yaml', epochs=5, imgsz=640, save_period=1, batch=-1, plots=True)
+    # model.train(data='./yolo_labels/train.yaml', epochs=5, imgsz=640, save_period=1, batch=-1, plots=True)
+    
+    
+    
+    if torch.cuda.is_available():
+        print('Using' ,torch.cuda.get_device_name(torch.cuda.current_device()))
+        model = YOLO('yolov8n.pt')
+        try:
+            results = model.train(data='datasets/coco.yaml', epochs=10, imgsz=640, save_period=1, batch=60, plots=True)
+        except:
+            gc.collect()
+            torch.cuda.empty_cache()
+    else:
+        print('cuda indisponible, ALLUMES LE GPU !!!')
