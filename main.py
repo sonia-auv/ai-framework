8b3cc0262397392b2d46fdc7f04a5af18b70cc4e
log.club.sonia <log.club.sonia@etsmtl.net>
2024-07-15 23:32:33 -0400

    ajustements

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from ultralytics import YOLO#, YOLOv10
+from ultralytics import YOLO
 import torch
 import argparse
 import yaml
@@ -30,10 +30,8 @@ class AiSonia():
             print('Cuda non disponible')
 
         # Loading model
-        if self.args.model in ['yolov8', 'yolov9']:
+        if self.args.model in ['yolov8', 'yolov9', 'yolov10']:
             self.model = YOLO(self.args.load_model)
-        # elif self.args.model == 'yolov10':
-        #     self.model = YOLOv10(self.args.load_model)
 
         if self.args.name is None:
             self.args.name = self.args.model
@@ -56,7 +54,7 @@ class AiSonia():
         parser.add_argument('--model', 
                             type=str, 
                             required=True, 
-                            choices=['yolov8', 'yolov9'],#, 'yolov10'], 
+                            choices=['yolov8', 'yolov9', 'yolov10'], 
                             help='Modèle choisi (défaut:yolov8)')
         parser.add_argument('--name', 
                             type=str, 
@@ -131,14 +129,21 @@ class AiSonia():
                 confidence = float(result.boxes.conf[i].item())*100
                 bounding_box = result.boxes.xyxy[i].cpu().numpy()
                 cv2.putText(img, 
-                            name+' '+"{:.1f}%".format(confidence), 
-                            (int(bounding_box[0])+10,int(bounding_box[1])), 
-                            cv2.FONT_HERSHEY_SIMPLEX, 
-                            1, (0,0,255), 2, 1)
+                            name, 
+                            (int((bounding_box[0]+5)),
+                             int((bounding_box[1]+bounding_box[3]-10)/2)), 
+                            cv2.FONT_HERSHEY_PLAIN, 
+                            .7, (0,0,255), 1, 1)
+                cv2.putText(img, 
+                            "{:.1f}%".format(confidence), 
+                            (int((bounding_box[0]+5)),
+                             int((bounding_box[1]+bounding_box[3]+10)/2)), 
+                            cv2.FONT_HERSHEY_PLAIN, 
+                            .7, (0,0,255), 1, 1)
                 cv2.rectangle(img, 
                             (int(bounding_box[0]),int(bounding_box[1])), 
                             (int(bounding_box[2]),int(bounding_box[3])), 
-                            (0,0,255), 2)
+                            (0,0,255), 1)
             cv2.imshow('frame', img)
             if cv2.waitKey(0) & 0xFF == ord('q'):
                 break
