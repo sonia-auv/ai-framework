31598cb7e490697d8b3c4c2b1120d7bc5fcef63a
Roberto Nightingale Castillo <roberto.nightingale@hotmail.com>
2026-02-05 19:02:59 -0500

    formatted main file with ruff

--- ai_sonia.py
+++ ai_sonia.py
@@ -6,32 +6,32 @@ from time import time
 from os import listdir
 from os.path import isfile, join
 
-CONFIG_DIR = 'config/'
-MODELS_DIR = 'models/'
-DATASET_DIR = 'datasets/'
-PATH_IMG_TEST = '/test/images'
+CONFIG_DIR = "config/"
+MODELS_DIR = "models/"
+DATASET_DIR = "datasets/"
+PATH_IMG_TEST = "/test/images"
 
-PARAMETERS_YAML = CONFIG_DIR + 'training_parameters.yaml'
-AUGMENT_YAML = CONFIG_DIR + 'augmentation.yaml'
-DEFAULT_YAML = CONFIG_DIR + '.default/ultralytics_default.yaml'
-TEMP_YAML = CONFIG_DIR + '.default/ultralytics_custom_config.yaml'
-DATASET_YAML = 'data.yaml'
+PARAMETERS_YAML = CONFIG_DIR + "training_parameters.yaml"
+AUGMENT_YAML = CONFIG_DIR + "augmentation.yaml"
+DEFAULT_YAML = CONFIG_DIR + ".default/ultralytics_default.yaml"
+TEMP_YAML = CONFIG_DIR + ".default/ultralytics_custom_config.yaml"
+DATASET_YAML = "data.yaml"
 
 
-class AiSonia():
+class AiSonia:
     def __init__(self, args):
         self.args = args
         # Detecting GPU
         if torch.cuda.is_available():
-            print('Cuda disponible')
+            print("Cuda disponible")
         else:
-            print('Cuda non disponible')
+            print("Cuda non disponible")
 
         # Loading model
-        if self.args.model in ['yolo'] and not self.args.load_model is None:
+        if self.args.model in ["yolo"] and not self.args.load_model is None:
             self.model = YOLO(self.args.load_model)
         elif self.args.load_model is None:
-            self.model = YOLO(MODELS_DIR + 'yolo/yolov8n.pt')
+            self.model = YOLO(MODELS_DIR + "yolo/yolov8n.pt")
 
         if self.args.name is None:
             self.args.name = self.args.model
@@ -50,18 +50,22 @@ class AiSonia():
             augment = yaml.full_load(open(AUGMENT_YAML))
             for key, value in augment.items():
                 settings[key] = value
-        yaml.dump(settings, open(TEMP_YAML, 'w'))
+        yaml.dump(settings, open(TEMP_YAML, "w"))
 
     def predict(self):
         test_path = self.args.dataset + PATH_IMG_TEST
-        img_list = [join(test_path, f) for f in listdir(test_path) if isfile(join(test_path, f))]
+        img_list = [
+            join(test_path, f) for f in listdir(test_path) if isfile(join(test_path, f))
+        ]
         t1 = time()
-        results = self.model.predict(img_list,
-                                     imgsz=self.args.inf_img_size,
-                                     conf=self.args.inf_confidence,
-                                     verbose=False)
+        results = self.model.predict(
+            img_list,
+            imgsz=self.args.inf_img_size,
+            conf=self.args.inf_confidence,
+            verbose=False,
+        )
         t2 = time()
-        print(1 / ((t2 - t1) / len(img_list)), 'fps')
+        print(1 / ((t2 - t1) / len(img_list)), "fps")
         for i, result in enumerate(results):
             img = cv2.imread(img_list[i])
             detection_count = result.boxes.shape[0]
@@ -70,30 +74,49 @@ class AiSonia():
                 name = result.names[cls]
                 confidence = float(result.boxes.conf[i].item()) * 100
                 bounding_box = result.boxes.xyxy[i].cpu().numpy()
-                cv2.putText(img,
-                            name,
-                            (int((bounding_box[0] + 5)),
-                             int((bounding_box[1] + bounding_box[3] - 10) / 2)),
-                            cv2.FONT_HERSHEY_PLAIN,
-                            .7, (0, 0, 255), 1, 1)
-                cv2.putText(img,
-                            "{:.1f}%".format(confidence),
-                            (int((bounding_box[0] + 5)),
-                             int((bounding_box[1] + bounding_box[3] + 10) / 2)),
-                            cv2.FONT_HERSHEY_PLAIN,
-                            .7, (0, 0, 255), 1, 1)
-                cv2.rectangle(img,
-                              (int(bounding_box[0]), int(bounding_box[1])),
-                              (int(bounding_box[2]), int(bounding_box[3])),
-                              (0, 0, 255), 1)
-            cv2.imshow('frame', img)
-            if cv2.waitKey(0) & 0xFF == ord('q'):
+                cv2.putText(
+                    img,
+                    name,
+                    (
+                        int((bounding_box[0] + 5)),
+                        int((bounding_box[1] + bounding_box[3] - 10) / 2),
+                    ),
+                    cv2.FONT_HERSHEY_PLAIN,
+                    0.7,
+                    (0, 0, 255),
+                    1,
+                    1,
+                )
+                cv2.putText(
+                    img,
+                    "{:.1f}%".format(confidence),
+                    (
+                        int((bounding_box[0] + 5)),
+                        int((bounding_box[1] + bounding_box[3] + 10) / 2),
+                    ),
+                    cv2.FONT_HERSHEY_PLAIN,
+                    0.7,
+                    (0, 0, 255),
+                    1,
+                    1,
+                )
+                cv2.rectangle(
+                    img,
+                    (int(bounding_box[0]), int(bounding_box[1])),
+                    (int(bounding_box[2]), int(bounding_box[3])),
+                    (0, 0, 255),
+                    1,
+                )
+            cv2.imshow("frame", img)
+            if cv2.waitKey(0) & 0xFF == ord("q"):
                 break
         cv2.destroyAllWindows()
 
     def train(self):
-        self.model.train(project=self.args.project,
-                         name=self.args.name,
-                         data=join(self.args.dataset, DATASET_YAML),
-                         resume=self.args.resume,
-                         cfg=TEMP_YAML)
+        self.model.train(
+            project=self.args.project,
+            name=self.args.name,
+            data=join(self.args.dataset, DATASET_YAML),
+            resume=self.args.resume,
+            cfg=TEMP_YAML,
+        )
